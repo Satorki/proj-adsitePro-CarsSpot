3aff07e0629965c1929af1dc9cd0e766f11c6be4
Satorki <sebastian.kedzier@gmail.com>
2024-09-03 14:37:17 +0200

    File structure created

---
+++ app/faq/page.tsx
@@ -0,0 +1,13 @@
+import Faq from "@/containers/faq/Faq";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      page faq
+      <Faq />
+    </div>
+  );
+};
+
+export default page;
